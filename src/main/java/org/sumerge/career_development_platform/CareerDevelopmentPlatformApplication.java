3dbb8ce54db8fc5a3cec79b00e686d6bba6653e2
mtolba <mm.ahmedd504@gmail.com>
2025-06-11 12:29:50 +0300

    Initial commit

---
+++ src/main/java/org/sumerge/career_development_platform/CareerDevelopmentPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.sumerge.career_development_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerDevelopmentPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerDevelopmentPlatformApplication.class, args);
+    }
+
+}
