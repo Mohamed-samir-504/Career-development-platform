9a9f53132dc29f61ac506f7e0bc2e773074b691f
mtolba <mm.ahmedd504@gmail.com>
2025-06-18 09:42:58 +0300

    Add CORS config and JWT filter

--- career-package-service/src/main/java/org/sumerge/careerpackageservice/CareerPackageServiceApplication.java
+++ career-package-service/src/main/java/org/sumerge/careerpackageservice/CareerPackageServiceApplication.java
@@ -2,8 +2,10 @@ package org.sumerge.careerpackageservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.sumerge.shared"})
 public class CareerPackageServiceApplication {
 
     public static void main(String[] args) {
