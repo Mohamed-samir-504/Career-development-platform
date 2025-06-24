fbe9f0ef284558487e8b034fb97c90cd4d4b64d1
Ahmed Hossam Mustafa <ahmustafa@sumerge.com>
2025-06-24 14:23:32 +0300

    removing hard-coded allowed origins and storing them in application properties

--- learning-service/src/main/java/org/sumerge/learningservice/config/WebConfig.java
+++ learning-service/src/main/java/org/sumerge/learningservice/config/WebConfig.java
@@ -1,5 +1,6 @@
 package org.sumerge.learningservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
@@ -7,13 +8,17 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 public class WebConfig {
 
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // allow all endpoints
-                        .allowedOrigins("http://localhost:4200") // adjust to your Angular dev server
+                registry.addMapping("/**")
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // optional if you send cookies/auth
