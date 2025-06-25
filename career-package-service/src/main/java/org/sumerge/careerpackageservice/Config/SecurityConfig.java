e87ecb20b98bd82aedc573836413d99e519b101f
mtolba <mm.ahmedd504@gmail.com>
2025-06-25 13:46:21 +0300

    Correct jwt filter package in SecurityConfig

--- career-package-service/src/main/java/org/sumerge/careerpackageservice/Config/SecurityConfig.java
+++ career-package-service/src/main/java/org/sumerge/careerpackageservice/Config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.sumerge.shared.utils.JwtAuthenticationFilter;
+import org.sumerge.shared.filter.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
