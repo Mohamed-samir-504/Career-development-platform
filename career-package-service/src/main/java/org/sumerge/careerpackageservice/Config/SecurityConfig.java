e64ec5853bfd50f86d8db986c4a1f6697dfc4fc5
mtolba <mm.ahmedd504@gmail.com>
2025-06-25 11:45:37 +0300

    Use shared jwt filter

--- career-package-service/src/main/java/org/sumerge/careerpackageservice/Config/SecurityConfig.java
+++ career-package-service/src/main/java/org/sumerge/careerpackageservice/Config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.sumerge.careerpackageservice.Filter.JwtAuthenticationFilter;
+import org.sumerge.shared.utils.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
