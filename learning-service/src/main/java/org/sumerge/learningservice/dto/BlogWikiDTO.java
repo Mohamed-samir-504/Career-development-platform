4c8b103db1386ae9df83c7ad57f1385af59ed032
Ahmed Hossam Mustafa <ahmustafa@sumerge.com>
2025-06-29 11:24:47 +0300

    fixed cors config and some entity and controller issues

--- learning-service/src/main/java/org/sumerge/learningservice/dto/BlogWikiDTO.java
+++ learning-service/src/main/java/org/sumerge/learningservice/dto/BlogWikiDTO.java
@@ -2,10 +2,13 @@ package org.sumerge.learningservice.dto;
 
 import lombok.*;
 
+import java.util.UUID;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class BlogWikiDTO {
+    private UUID id;
     private String title;
     private String content;
     private String attachmentId;
