d66e2a86f937c081c95af28122edf381ed0e431f
mtolba <mm.ahmedd504@gmail.com>
2025-07-07 17:18:49 +0300

    Renaming service and controller files

--- career-package-service/src/main/java/org/sumerge/careerpackageservice/Controller/UserSectionSubmissionController.java
+++ career-package-service/src/main/java/org/sumerge/careerpackageservice/Controller/UserSectionSubmissionController.java
@@ -4,7 +4,7 @@ package org.sumerge.careerpackageservice.Controller;
 import org.springframework.http.ResponseEntity;
 import org.sumerge.careerpackageservice.Dto.Request.SubmitUserSectionRequest;
 import org.sumerge.careerpackageservice.Entity.UserSectionSubmission;
-import org.sumerge.careerpackageservice.Service.UserSectionResponseService;
+import org.sumerge.careerpackageservice.Service.UserSectionSubmissionService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,37 +13,37 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/user-section-response")
-public class UserSectionResponseController {
+public class UserSectionSubmissionController {
 
 
-    private final UserSectionResponseService userSectionResponseService;
+    private final UserSectionSubmissionService userSectionSubmissionService;
 
-    public UserSectionResponseController(UserSectionResponseService userSectionResponseService) {
-        this.userSectionResponseService = userSectionResponseService;
+    public UserSectionSubmissionController(UserSectionSubmissionService userSectionSubmissionService) {
+        this.userSectionSubmissionService = userSectionSubmissionService;
     }
 
     @GetMapping
     public List<UserSectionSubmission> getAll() {
-        return userSectionResponseService.getAll();
+        return userSectionSubmissionService.getAll();
     }
 
     @GetMapping("/{id}")
     public Optional<UserSectionSubmission> getById(@PathVariable UUID id) {
-        return userSectionResponseService.getById(id);
+        return userSectionSubmissionService.getById(id);
     }
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable UUID id) {
-        userSectionResponseService.delete(id);
+        userSectionSubmissionService.delete(id);
     }
 
     @PostMapping
     public ResponseEntity<UserSectionSubmission> submit(@RequestBody SubmitUserSectionRequest request) {
-        return ResponseEntity.ok(userSectionResponseService.submitSection(request));
+        return ResponseEntity.ok(userSectionSubmissionService.submitSection(request));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<UserSectionSubmission> update(@PathVariable UUID id, @RequestBody SubmitUserSectionRequest request) {
-        return ResponseEntity.ok(userSectionResponseService.updateSection(id, request));
+        return ResponseEntity.ok(userSectionSubmissionService.updateSection(id, request));
     }
 }
