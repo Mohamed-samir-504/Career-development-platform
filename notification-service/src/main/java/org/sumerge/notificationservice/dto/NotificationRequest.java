df3b7e34f72e5843fd4eb77bfe7e34a5afa4c343
mtolba <mm.ahmedd504@gmail.com>
2025-07-09 18:22:13 +0300

    Add noArgsConstructor to NotificationRequest

--- notification-service/src/main/java/org/sumerge/notificationservice/dto/NotificationRequest.java
+++ notification-service/src/main/java/org/sumerge/notificationservice/dto/NotificationRequest.java
@@ -2,11 +2,13 @@ package org.sumerge.notificationservice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class NotificationRequest {
     private UUID senderId;
     private UUID receiverId;
